2248adca5d23c7b14ebb65ab935e131d9d94350b
Lakshmi Manikanteswar Punnam <lakshmi.punnam.18@mountblue.tech>
2021-11-13 10:28:07 +0530

    converted registration_count_by_pba_by_yor.py file into more readable

--- Code/registration_count_by_pba_by_yor.py
+++ Code/registration_count_by_pba_by_yor.py
@@ -1,28 +1,30 @@
-"""This Program will display the Grouped Bar Chart of Number of registrations
-done in each Principal Business Activity in Each Year.
-It is divided into 4 modules:
-    1.Map year to registration count by pba.
-    2.Map pba to registration count by year.
-    (deleting some years which have low registration count).
-    3.Sort registration count by year.
-    4.Plot the resulting data.
+"""This Program will display the Grouped Bar Chart of Top 5
+Principal business activites in last 10 years.
+It is divided into 3 modules:
+    1.Map pba to registration count from last 10 years data.
+    2.get top 5 pbas and order them based on increasing order of year.
+    3.Plot the resulting data.
 Note: pba means principal business activity.
 """
+
 # importing libraries to deal with csv files and to plot the data.
 import os
 import csv
 from matplotlib import pyplot as plt
 
-def map_year_to_registration_count_by_pba(filename):
-    """It will create a dictionary that maps Each year to
-    registration count by corresponding principal business activity
+LAST_10_YEARS = [str(year) for year in range(2012,2022)]
+
+def map_pba_to_registration_count_by_year(filename):
+    """It will create a dictionary that maps principal business activity
+    to registration count from last 10 years data, and will return
+    that dictionary.
     """
+    pba_to_registration_count = {}
     with open(filename,"r",encoding = "ISO-8859-1") as file_obj:
-        reader = csv.reader(file_obj) # skipping the headers.
-        next(reader,None)
-        for row in reader:
+        company_registration_reader = csv.DictReader(file_obj)
+        for current_registration_info in company_registration_reader:
             try:
-                date,_,year = row[6].strip().split('-')
+                date,_,year = current_registration_info["DATE_OF_REGISTRATION"].strip().split('-')
                 if len(date)==4:
                     year = date
                 elif int(year) in range(0,22):
@@ -32,71 +34,56 @@ def map_year_to_registration_count_by_pba(filename):
             except ValueError:
                 continue
             else:
-                if year not in registration_count_by_year:
-                    registration_count_by_year[year] = {}
-                principal_business_activity = row[11].strip()
-                count = registration_count_by_year[year].get(principal_business_activity,0) + 1
-                registration_count_by_year[year][principal_business_activity] = count
+                if int(year) in range(2012,2022):
+                    principal_business_activity = \
+                    current_registration_info["PRINCIPAL_BUSINESS_ACTIVITY_AS_PER_CIN"].strip()
+                    if principal_business_activity not in pba_to_registration_count:
+                        pba_to_registration_count[principal_business_activity] = {}
+                    count = pba_to_registration_count[principal_business_activity].get(year,0) + 1
+                    pba_to_registration_count[principal_business_activity][year] = count
+    return pba_to_registration_count
 
-def update_registration_count_by_pba(pba_s,year):
-    """It will update the registration count by principal business activity"""
-    for pba,count in pba_s.items():
-        if pba not in registration_count_by_pba:
-            registration_count_by_pba[pba] = {}
-        registration_count_by_pba[pba][year] = count
-
-def map_pba_to_registration_count_by_year():
-    """It will create a dictionary that maps principal business activity
-    to registration count by year
+def get_top_5_pba_s(pba_to_registration_count):
+    """It will fetch the top 5 principal business activities based on
+    the no.of the registrations done over last 10 years.
     """
-    for year,pba_s in registration_count_by_year.items():
-        total_registrations = sum(pba_s.values())
-        if total_registrations > 3000:
-            years.add(year)
-            update_registration_count_by_pba(pba_s,year)
-
-def sort_registration_count_based_on_year():
-    """It will create a dictionary that maps principal business activity
-    to registration count sorted by year
-    """
-    for pba,reg_count_by_year in registration_count_by_pba.items():
-        registration_count_by_pba_ordered[pba] = []
-        for year in sorted_years:
-            count = reg_count_by_year.get(year,0)
-            registration_count_by_pba_ordered[pba].append(count)
-
-def plot_data():
+    pba_to_registration_count_ordered = {}
+    pba_names = list(pba_to_registration_count.keys())
+    pba_names.sort(key=lambda name:sum(pba_to_registration_count[name].values()),reverse=True)
+    top_5_pba_names = pba_names[:5][::-1]
+    # Ordering the top 5 pba_s in the increasing order of year
+    for pba in top_5_pba_names:
+        pba_to_registration_count_ordered[pba] = []
+        for year in LAST_10_YEARS:
+            registration_count = pba_to_registration_count[pba].get(year,0)
+            pba_to_registration_count_ordered[pba].append(registration_count)
+    return pba_to_registration_count_ordered,top_5_pba_names
+def plot_data(pba_to_registration_count_ordered,top_5_pba_names):
     """Creating and displaying the Bar Chart"""
-    no_years = len(sorted_years)
+    no_years = 10
     prev_start = list(range(0,no_years))
-    bar_width=0.2
-    fig = plt.figure()
-    fig.set_figheight(12)
-    fig.set_figwidth(12)
-    fig_obj = fig.add_subplot(111)
-    # Shrink current axis by 20%
-    box = fig_obj.get_position()
-    fig_obj.set_position([box.x0, box.y0, box.width * 0.8, box.height])
-    for pba,reg_count in registration_count_by_pba_ordered.items():
-        label_name = "".join([w[0] for w in pba.split(" ")])
-        fig_obj.bar(prev_start,reg_count,width=bar_width,label=label_name)
+    bar_width=0.1
+    figure_width,figure_height = 15,10
+    plt.figure(figsize=(figure_width,figure_height))
+    for pba in top_5_pba_names:
+        registration_count_by_year = pba_to_registration_count_ordered[pba]
+        label_name = " ".join(pba.strip().split(" ")[:2])
+        plt.bar(prev_start,registration_count_by_year,width=bar_width,label=label_name)
         prev_start = [i+bar_width for i in prev_start]
     # Adding xtick labels
-    start_pos = [i+2 for i in range(no_years)]
-    plt.xticks(start_pos,sorted_years)
-    # Put a legend to the right of the current axis
-    fig_obj.legend(loc='center left', bbox_to_anchor=(1, 0.5))
-    plt.title("Number of registrations done in each Principal Business Activity in Each Year")
-    plt.xlabel("Year")
-    plt.ylabel("Number of registrations")
+    start_pos = [i for i in range(no_years)]
+    plt.xticks(start_pos,LAST_10_YEARS)
+    plt.legend(loc='upper right')
+    plt.title("Top 5 principal business activities in last 10 years",fontweight="bold")
+    plt.xlabel("Year",fontweight="bold")
+    plt.ylabel("Number of registrations",fontweight="bold")
     plt.show()
 
-registration_count_by_year = {}
-registration_count_by_pba = {}
-years = set()
-map_year_to_registration_count_by_pba(os.getcwd()+"/../Data/Maharashtra.csv")
-map_pba_to_registration_count_by_year()
-sorted_years = sorted(list(years))
-registration_count_by_pba_ordered = {}
-sort_registration_count_based_on_year()
-plot_data()
+def execute():
+    """It will call all the helper functions to achieve our aim"""
+    file_path = os.getcwd()+"/../Data/Maharashtra.csv"
+    pba_to_registration_count = map_pba_to_registration_count_by_year(file_path)
+    pba_to_registration_count_ordered,top_5_pba_names = get_top_5_pba_s(pba_to_registration_count)
+    plot_data(pba_to_registration_count_ordered,top_5_pba_names)
+
+execute()
